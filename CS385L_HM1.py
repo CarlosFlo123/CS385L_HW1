bd49222847368cf50f297bc672ade75da7e0fdae
Carlos <carlos@Carloss-MBP.hsd1.ca.comcast.net>
2020-01-10 17:16:44 -0800

    breathing Python

---
+++ CS385L_HM1.py
@@ -0,0 +1,45 @@
+#CS385L: Python Programming
+#Professor: Dr. Bhaskar, Vidhyacharan
+#Student: Carlos Flores Valero (19537)
+#Description: Homework 1 | Week 1
+
+#1________________________________________________________________________
+message = "Hello World"
+print(message)
+message = "Hello Cruel World"
+print(message)
+
+#2________________________________________________________________________
+#2-5
+quote = 'Edsger Dijkstra once said, "Computer Science is no more about \ncomputers than astronomy is about telescopes."'
+print(quote)
+
+#2-6
+famous_person = "Edsger Dijkstra"
+quote = '"Computer Science is no more about \ncomputers than astronomy is about telescopes."'
+message = famous_person + " once said, " + quote
+print(message)
+
+#2-7
+name = "  \t\t   Edsger Dijkstra    \n\n"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+#3________________________________________________________________________
+favNumber = input("Insert your number: ")
+message = "Favorite number: " + str(favNumber)
+print(message)
+
+#4________________________________________________________________________
+#Showing the versatility that Python offers with the single and double quotation marks.
+quote = 'Edsger Dijkstra once said, "Computer Science is no more about \ncomputers than astronomy is about telescopes."'
+print(quote)
+
+#Carlos Flores Valero | 01/09/2020  | Program that modifies a variable and prints it again.
+message = "Hello World"
+print(message)
+message = "Hello Cruel World"
+print(message)
+
